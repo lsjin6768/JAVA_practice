0c4a098a2e554875d89e99e79fe7903fa1f9c53e
USER <USER@DESKTOP-UQ4G7JE>
2022-10-11 13:50:25 +0900

    construct

---
+++ chapter1/src/ch06/Student.java
@@ -0,0 +1,18 @@
+package ch06;
+
+public class Student {
+	
+	public int studentNumber;
+	public String studentName;
+	public int grade;
+	public Student(){}
+	public Student(int studentNumber , String studentName, int grade) {
+		this.studentNumber = studentNumber;
+		this.studentName=studentName;
+		this.grade=grade;
+	}
+	public String showStudentInfo() {
+		return studentName + "'s student number is "+ studentNumber + " and grade is "+ grade;
+		
+	}
+}
